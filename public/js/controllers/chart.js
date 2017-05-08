879fbd52fd577a6ac40cc0e1078da8a08cf996de
grozdanovgg <grozdanovgg@gmail.com>
2017-05-08 03:32:43 +0300

    Try to clean up the mess I`ve made

--- public/js/controllers/chart.js
+++ public/js/controllers/chart.js
@@ -1,15 +1,38 @@
 import { template } from '../template.js';
 import $ from 'jquery';
-
+import { getChartData } from 'data';
+import { createCompleteChart } from 'chartPainter';
 
 const chartController = {
     get() {
         template.get('chart')
             .then(template => {
                 $('#contents').html(template());
-                toastr.success("Chart loaded!");
-            });
+                $("#search-container").css('margin-top', '15px');
+                $('#search-button').on('click', () => {
+                    const ticker = $("#ticker").val();
+                    const number = $("#period").val();
+                    const type = $('input[name=periodType]:checked').val();
+                    const period = {
+                        number: +number,
+                        type: type
+                    }
+
+                    let svg = $('svg');
+                    console.log(svg.length)
+                    if (svg.length != 0) {
+                        svg.remove();
+                    }
+                    // TODO: add validation (all input required)!
+                    getChartData(ticker, period)
+                        .then((data) => {
+                            createCompleteChart(data);
+                            toastr.success("Chart loaded!");
+                        });
+                })
+
+            })
     }
-};
+}
 
 export { chartController };
--- public/js/controllers/homepage/sidebar-router.js
+++ public/js/controllers/homepage/sidebar-router.js
@@ -1,22 +1,17 @@
 import * as data from '../../data/data.js';
 import { template } from '../../template.js';
 import { Index } from '../../data/indexClass.js';
-import { SideBar } from '../../data/sidebarClass.js';
-import { listController } from 'listController';
+import { SideBar } from './sidebarClass.js';
 import $ from 'jquery';
 
-//Mockup data array:
-const arr = [5, 5, 6, 6, 8, 4, 3, 1, 4, 4, 4, 5, 3, 5, 6, 4, 3, 1, 3,
-    6, 7, 3, 5, 5, 6, 6, 8, 4, 3, 1, 4, 4, 4, 5, 3, 5, 6, 4, 3, 8, 3,
-    6, 7, 3, 5, 5, 6, 6, 8, 4, 3, 1, 4, 4, 4, 5, 3, 5, 6, 4, 3, 12, 3,
-    6, 7, 3, 5, 5, 6, 6, 8, 4, 3, 1, 4, 4, 4, 5, 3, 5, 6, 4, 3, 5, 3,
-]
+//Export to individual module!
+
 
 //Mockup input data. To be extracted from Yahoo
-let index1 = new Index('Aple', 50, 'AAPL', data.scoreAnalytics(arr).totalPoints),
-    index2 = new Index('Google', 60, 'GOOG', data.scoreAnalytics(arr).totalPoints),
-    index3 = new Index('Yahoo', 70, 'YHOO', data.scoreAnalytics(arr).totalPoints),
-    index4 = new Index('IBM', 20, 'IBM', data.scoreAnalytics(arr).totalPoints);
+let index1 = new Index('Aple', 50, 'AAPL', 1000),
+    index2 = new Index('Google', 60, 'GOOG', 20000),
+    index3 = new Index('Yahoo', 70, 'YHOO', 3000),
+    index4 = new Index('IBM', 20, 'IBM', 150);
 let favoritesList = [index1, index2, index3, index4];
 
 const sideBar = new SideBar();
@@ -26,11 +21,7 @@ const sideBarContent = {
         if (params) {
             switch (params.sideBarContent) {
                 case ':favorites':
-                    sideBar.callFavorites(favoritesList);
-                    break;
-                case ':search':
-                    console.log('here is Search');
-                    listController.get();
+                    sideBar.callFavorites()
                     break;
                 case ':top-ten':
                     sideBar.callTopTen()
@@ -42,8 +33,10 @@ const sideBarContent = {
                     sideBar.callNews()
                     break;
                 default:
-                    sideBar.callFavorites(favoritesList);
+                    sideBar.callFavorites();
             }
+        } else {
+            sideBar.callFavorites();
         }
     }
 }
